24ab1068d5b8d3ad3627860487059def77de5f4b
Robert Kirkman <therobertkirkman@gmail.com>
2022-05-13 19:43:53 +0900

    cleaned up Health

--- src/Health/Health.js
+++ src/Health/Health.js
@@ -17,12 +17,30 @@ function Health(props) {
 }
 
 Health.propTypes = {
+  /**
+   * @uxpinignoreprop
+   */
   children: PropTypes.node, 
-  textSize: PropTypes.oneOf(["xs", "s", "m", "l", "xxxs", "xxs"]),
-  color: PropTypes.oneOf(["default", "accent", "success", "warning", "danger", "ghost", "subdued"]),
+
+  /**
+   * Defines a string value that labels the current element. See aria-labelledby.
+   */
   "aria-label": PropTypes.string,
+
   "data-test-subj": PropTypes.string,
+
   className: PropTypes.string,
+
+  /**
+   * Matches the text scales of EuiText. The inherit style will get its font size from the parent element
+   */
+  textSize: PropTypes.oneOf(["xs", "s", "m", "l", "xxxs", "xxs"]),
+
+  /**
+   * Sets the color of the dot icon. It accepts any IconColor: default, primary, success, accent, warning, danger, text, subdued or ghost; or any valid CSS color value as a string
+   */
+  color: PropTypes.oneOf(["default", "accent", "success", "warning", "danger", "ghost", "subdued"]),
+  
 };
 
 export default Health;
