b29b49c350d49ffa745609943358f0e5445a9f88
Robert Kirkman <therobertkirkman@gmail.com>
2022-05-13 20:11:42 +0900

    cleaned up Card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -15,39 +15,94 @@ function Card(props) {
       icon={props.iconType ? <Icon type={props.iconType} /> : null}
       >
         {props.children}
-        </EuiCard>
+      </EuiCard>
   );
 }
 
 Card.propTypes = {
+  /**
+   * Custom children
+   * @uxpinignoreprop
+   */
   children: PropTypes.node,
-  title: PropTypes.node,
-  footer: PropTypes.node,
-  title: PropTypes.node,
-  titleElement: PropTypes.oneOf(["h2", "h3", "h4", "h5", "h6", "span"]),
-  titleSize: PropTypes.oneOf(["xs", "s"]),
-  description: PropTypes.node,
-  image: PropTypes.string,
-  textAlign: PropTypes.oneOf(["left", "right", "center"]),
-  layout: PropTypes.oneOf(["horizontal", "vertical"]),
-  heading: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6", "p"]),
-  color: PropTypes.oneOf(["primary", "accent", "success", "warning", "danger"]),
-  size: PropTypes.oneOf(["s", "m"]),
-  iconType: PropTypes.oneOf(iconList),
-  isDisabled: PropTypes.bool,
-  href: PropTypes.string,
-  rel: PropTypes.string,
-  betaBadgeProps: PropTypes.object,
-  display: PropTypes.oneOf(["primary", "accent", "success", "warning", "danger", "subdued", "plain", "transparent"]),
-  selectable: PropTypes.Object, // EuiCardSelectProps e.g - selectable={{ onClick: card2Clicked, isSelected: card2Selected }}
-  hasBorder: PropTypes.bool,
+
+  /**
+   * Defines a string value that labels the current element. See aria-labelledby.
+   */
   "aria-label": PropTypes.string,
+
   "data-test-subj": PropTypes.string,
+
   className: PropTypes.string,
-  ref: PropTypes.element,
-  onClick: PropTypes.func,
+
+  href: PropTypes.string,
+
+  rel: PropTypes.string,
+
+  /**
+   * Accepts a url in string form or ReactElement for a custom image component
+   */
+  image: PropTypes.string,
+
+  /**
+   * Adds a badge to top of the card to label it as "Beta" or other non-GA state. Accepts all the props of EuiBetaBadge, where label is required.
+   */
+  betaBadgeProps: PropTypes.object,
+
+  /**
+   * Adds a button to the bottom of the card to allow for in-place selection
+   */
+  selectable: PropTypes.bool, // EuiCardSelectProps e.g - selectable={{ onClick: card2Clicked, isSelected: card2Selected }}
+
+  /**
+   * Cards are required to have at least a title and a description and/or children
+   */
+  title: PropTypes.node,
+
+  /**
+   * Accepts any combination of elements
+   */
+  footer: PropTypes.node,
+
+  /**
+   * Placed within a small EuiText <p> tag
+   */
+  description: PropTypes.node,
+
+  /**
+   * Determines the title's heading element
+   */
+  titleElement: PropTypes.oneOf(["h2", "h3", "h4", "h5", "h6", "span"]),
+
+  /**
+   * Determines the title's size, matching that of EuiTitle. Though, card titles can't be too large or small relative to the description text.
+   */
+  titleSize: PropTypes.oneOf(["xs", "s"]),
+
+  /**
+   * Changes alignment of the title and description
+   */
+  textAlign: PropTypes.oneOf(["left", "right", "center"]),
+
+  /**
+   * Change to "horizontal" if you need the icon to be left of the content. Horizontal layouts cannot be used in conjunction with image, footer, or textAlign.
+   */
+  layout: PropTypes.oneOf(["horizontal", "vertical"]),
+
+
+  iconType: PropTypes.oneOf(iconList),
+
+  /**
+   * Matches to the color property of EuiPanel. If defined, removes any border & shadow. Leave as undefined to display as a default panel. Selectable cards will always display as a default panel
+   */
+  display: PropTypes.oneOf(["primary", "accent", "success", "warning", "danger", "subdued", "plain", "transparent"]),
+
+  isDisabled: PropTypes.bool,
+
+  /**
+   * Use a border style of card instead of shadow
+   */
+  hasBorder: PropTypes.bool,
 };
-
-
 
 export default Card;
--- src/Card/presets/0-default.jsx
+++ src/Card/presets/0-default.jsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 
 import Card from "../Card";
+import Text from "../../Text/Text";
 
 export default (
     <Card
+        uxpId="card1"
         title="title"
         description="description"
-    />
+    >
+        <Text uxpId="text1">Text component</Text>
+    </Card>
 );
