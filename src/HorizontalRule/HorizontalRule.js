e8088f4d1d5d9e803cf205798c146513a2e2e93c
Robert Kirkman <therobertkirkman@gmail.com>
2022-05-13 19:47:04 +0900

    cleaned up HorizontalRule

--- src/HorizontalRule/HorizontalRule.js
+++ src/HorizontalRule/HorizontalRule.js
@@ -10,18 +10,27 @@ function HorizontalRule(props) {
     <EuiHorizontalRule 
       {...props} 
       aria-Label={props.ariaLabel}
-      data-test-subj={props.dataTestSubj}>
-        {props.children}
-    </EuiHorizontalRule>
+      data-test-subj={props.dataTestSubj} />
   );
 }
 
 HorizontalRule.propTypes = {
-  size: PropTypes.oneOf(["full", "half", "quarter"]), 
-  textSize: PropTypes.oneOf(["xs", "s", "m", "l", "xl", "none", "xxl"]),
   "aria-label": PropTypes.string,
+
   "data-test-subj": PropTypes.string,
+
   className: PropTypes.string,
+
+  /**
+   * Size of the Horizontal line
+   */
+  size: PropTypes.oneOf(["full", "half", "quarter"]), 
+
+  /**
+   * Margin of the horizontal line
+   */
+  margin: PropTypes.oneOf(["xs", "s", "m", "l", "xl", "none", "xxl"]),
+  
 };
 
 export default HorizontalRule;
